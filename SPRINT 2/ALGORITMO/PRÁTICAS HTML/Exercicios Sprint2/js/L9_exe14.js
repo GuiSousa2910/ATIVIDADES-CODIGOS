fdf083c77f2d0e7187d0483c2c97b45d266e2a92
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-04-30 16:14:44 -0300

    l11 exe 8

--- SPRINT 2/ALGORITMO/PRÁTICAS HTML/Exercicios Sprint2/js/L9_exe14.js
+++ SPRINT 2/ALGORITMO/PRÁTICAS HTML/Exercicios Sprint2/js/L9_exe14.js
@@ -1,30 +1,40 @@
 function mostrar() {
-    var frase = '';
-    var idade = Number(input_idade.value);
+    // var frase = '';
+    // var idade = Number(input_idade.value);
 
-    for (var contador = 0; contador <= idade; contador++) {
-        if (contador >= 0 && contador <= 4) {
-            frase += `${contador} anos (Primeira Infância)<br>`;
-        }
-        else if (contador > 4 && contador <= 6) {
-            frase += `${contador} anos (Pré-escolar)<br>`;
-        }
-        else if (contador > 6 && contador <= 10) {
-            frase += `${contador} anos (Segunda infância)<br>`;
-        }
-        else if (contador > 10 && contador <= 18) {
-            frase += `${contador} anos (Adolescência)<br>`;
-        }
-        else if (contador > 18 && contador <= 50) {
-            frase += `${contador} anos (Adulto)<br>`;
-        }
-        else if (contador > 50 && contador <= 100) {
-            frase += `${contador} anos (Melhor idade)<br>`;
-        }
-        else if (contador > 100) {
-            frase += `${contador} anos (Matusalém)<br>`;
-        }
+    // for (var contador = 0; contador <= idade; contador++) {
+    //     if (contador >= 0 && contador <= 4) {
+    //         frase += `${contador} anos (Primeira Infância)<br>`;
+    //     }
+    //     else if (contador > 4 && contador <= 6) {
+    //         frase += `${contador} anos (Pré-escolar)<br>`;
+    //     }
+    //     else if (contador > 6 && contador <= 10) {
+    //         frase += `${contador} anos (Segunda infância)<br>`;
+    //     }
+    //     else if (contador > 10 && contador <= 18) {
+    //         frase += `${contador} anos (Adolescência)<br>`;
+    //     }
+    //     else if (contador > 18 && contador <= 50) {
+    //         frase += `${contador} anos (Adulto)<br>`;
+    //     }
+    //     else if (contador > 50 && contador <= 100) {
+    //         frase += `${contador} anos (Melhor idade)<br>`;
+    //     }
+    //     else if (contador > 100) {
+    //         frase += `${contador} anos (Matusalém)<br>`;
+    //     }
+    // }
+    // divMensagem.innerHTML = frase;
+
+    var frase = '';
+    var nome = input_idade.value;
+
+    if (nome == 'abigail') {
+        frase += 'abigail'
+    }
+    else if (nome == 'amanda') {
+        frase += 'amanda'
     }
     divMensagem.innerHTML = frase;
-
 }
--- SPRINT 3/ALGORITMO/PRÁTICAS HTML/Exercicios Sprint 3/js/L11_exe3.js
+++ SPRINT 3/ALGORITMO/PRÁTICAS HTML/Exercicios Sprint 3/js/L11_exe3.js
@@ -1,7 +1,6 @@
 var lista = [];
 function cadastrar() {
     var bairro = input_bairro.value;
-
     lista.push(bairro);
 }
 
@@ -9,8 +8,8 @@ function relembrar() {
     var numero = Number(input_numero.value);
 
     if (numero > lista.length) {
-        divMensagem.innerHTML = ''
-        divMensagem.innerHTML = `Você ainda não morou num ${numero}º bairro!`
+        divMensagem.innerHTML = '';
+        divMensagem.innerHTML = `Você ainda não morou num ${numero}º bairro!`;
     }
     divMensagem.innerHTML += `${lista[numero]}`;
 }
