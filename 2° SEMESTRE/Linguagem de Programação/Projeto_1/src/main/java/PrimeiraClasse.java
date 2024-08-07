1f33a675564fba9dea8348695b40379b261f06e3
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2024-08-07 20:45:49 -0300

    Iniciando aula de LP

---
+++ 2° SEMESTRE/Linguagem de Programação/Projeto_1/src/main/java/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+
+        String nome = "Guilherme";
+        Integer idade = 42;
+        Double altura = 1.81;
+
+        System.out.println("Olá " + nome + ", sua idade é: " + idade + " e uma altura de " + altura + " metros");
+    }
+}
