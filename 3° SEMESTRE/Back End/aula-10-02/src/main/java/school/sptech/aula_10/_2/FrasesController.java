d6b1e39a4f6a9d3f46e7be45686de455ffa12ccb
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2025-02-10 18:00:34 -0300

    endpoints

---
+++ 3° SEMESTRE/Back End/aula-10-02/src/main/java/school/sptech/aula_10/_2/FrasesController.java
@@ -0,0 +1,21 @@
+package school.sptech.aula_10._2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrasesController {
+
+    @GetMapping("/frases/saudacao")
+    public String saudacao(){
+        return "Olá, Mundo!";
+    }
+
+    @GetMapping("/frases/ola/{nome}")
+    public String saudacao(@PathVariable String nome){
+        return "Olá %s".formatted(nome);
+    }
+
+
+
+}
---
+++ 3° SEMESTRE/Back End/aula-10-02/src/test/java/school/sptech/aula_10/_2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.aula_10._2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
