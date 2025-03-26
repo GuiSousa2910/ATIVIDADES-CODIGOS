8867401bfd191a277f906ca07bda54b56d2250d6
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2025-03-26 19:12:53 -0300

    erro corrigido

--- 3° SEMESTRE/Front End/AULA/aula-19-03/React/meu-primeiro-projeto/src/App.jsx
+++ 3° SEMESTRE/Front End/AULA/aula-19-03/React/meu-primeiro-projeto/src/App.jsx
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
 import "./App.css";
 import Card from "./Card";
 
 function App() {
 
+  const [contador, setContador] = useState(0);
+
+  const contarNumero = () => {
+    if (contador < 30) {
+      setContador(contador + 1);
+    } else {
+      setContador("Limite atingido");
+    }
+  };
+
   return (
     <>
-      <Card titulo={""} paragrafo={"Ele é legal"}/>
-      <Card titulo={"Pedro"} paragrafo={"Ele é chato"}/>
-      <Card titulo={"Gui"} paragrafo={"Ele é doido"}/>
+      {/* <Card titulo={""} paragrafo={"Ele é legal"}/> */}
+      {/* <Card titulo={"Pedro"} paragrafo={"Ele é chato"}/> */}
+      {/* <Card titulo={"Gui"} paragrafo={"Ele é doido"}/> */}
+      <button onClick={contarNumero}>
+        Contar
+      </button>
+      <p>{contador}</p>
     </>
   );
 }
