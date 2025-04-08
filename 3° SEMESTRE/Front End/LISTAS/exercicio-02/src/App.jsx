9bff602a1bca265e736156ac13d7a975a1d88e62
Guilherme Sousa <guilherme.agostinho@sptech.school>
2025-04-08 09:52:15 -0300

    finalizando atividade

--- 3° SEMESTRE/Front End/LISTAS/exercicio-02/src/App.jsx
+++ 3° SEMESTRE/Front End/LISTAS/exercicio-02/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       {/* <Contador /> */}
       {/* <Calculadora /> */}
       {/* <DiarioInterativo /> */}
-      <ListaTelefonica />
-      {/* <ExibirPersonagem /> */}
+      {/* <ListaTelefonica /> */}
+      <ExibirPersonagem />
     </>
   )
 }
--- 3° SEMESTRE/Front End/LISTAS/exercicio-02/src/components/ExibirPersonagens.jsx
+++ 3° SEMESTRE/Front End/LISTAS/exercicio-02/src/components/ExibirPersonagens.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export function ExibirPersonagem() {
 
+  const [personagem, setPersonagem] = useState({
+    nome: "",
+    imagem: ""
+  });
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/502")
+      .then((response) => response.json())
+      .then(json => {
+        console.log(json);
+        setPersonagem({
+          nome: json.name,
+          imagem: json.image
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
-      <h1>Personagens</h1>
+      <h1>Personagem</h1>
       <ul>
-       <li>Nome do personagem: ___ Super poder: ___</li>
+        <li>Nome do personagem: {personagem.nome} Imagem: {<img src={personagem.imagem} />}</li>
       </ul>
     </div>
   );
