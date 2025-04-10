94ee18f146834e1b2771452f69a9643645a018bc
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2025-04-10 17:35:36 -0300

    Aprendendo sobre axios

---
+++ 3° SEMESTRE/Front End/AULA/axios/projeto-axios/src/App.jsx
@@ -0,0 +1,56 @@
+import {useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+import { api } from './provider/apiInstance';
+
+function App() {
+  useEffect(() => { }, []);
+
+  function adicionarUsers() {
+    api.post("/usuarios", {
+      nome: "Lucas",
+      idade: 23,
+      profissao: "Desenvolvedor"
+    });
+  }
+
+  const buscarUsers = () => {
+    api.get("/usuarios")
+      .then(respose => {
+        console.log(respose.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const atualizarUsers = () => {
+    api.put("/usuarios/4f07", {
+      nome: "Marcos",
+      idade: 23,
+      profissao: "Desenvolvedor"
+    });
+  };
+
+  const deletarUsers = () => {
+    api.delete("/usuarios/4f07")
+      .then(respose => {
+        console.log(respose.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+
+  return (
+    <>
+      <button onClick={adicionarUsers}>ADICIONAR</button>
+      <button onClick={buscarUsers}>BUSCAR</button>
+      <button onClick={atualizarUsers}>ATUALIZAR</button>
+      <button onClick={deletarUsers}>DELETAR</button>
+    </>
+  );
+}
+
+export default App;
